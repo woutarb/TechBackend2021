29c367ee9ae0a7244179d589b11a551ac88a8d61
Wouter Bijnsdorp <wouterb98@gmail.com>
2021-02-09 14:23:28 +0100

    Initial basic routing post-testing

--- index.js
+++ index.js
@@ -1,15 +1,42 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+const express = require('express')
+const app = express()
+const port = 3000
 
 app.get('/',(req, res) =>{
     res.send("Hello World")
 });
 
-app.listen(port, ()=>{
-    console.log('Example app listening on port ${port}!')
+app.get('/profile',(req, res) =>{
+    res.send("Your profile")
 });
 
-app.arguments(function (req, res, next){
-    res.status(404).send("Sorrt, can't find it!")
+app.get('/profile/:',(req, res) =>{
+    res.send(`Profile of user ${req.params.movieID}`)
 });
+
+app.listen(port, ()=>{
+    console.log(`Example app listening on port ${port}!`)
+});
+
+app.use(function (req, res, next){
+    res.status(404).send("Error 404 - Sorry, can't find this page for you!")
+});
+
+
+
+/*
+  //Respond to a Post request to the /user route:
+app.post('/', function (req, res) {
+    res.send('Got a POST request')
+  })
+  //Respond to a PUT request to the /user route:
+  
+  app.put('/user', function (req, res) {
+    res.send('Got a PUT request at /user')
+  })
+  //Respond to a DELETE request to the /user route:
+  
+  app.delete('/user', function (req, res) {
+    res.send('Got a DELETE request at /user')
+  })
+  */
