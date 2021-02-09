2ca9fd44d9b8e16716f2466f63aadfa07a4f6e8b
Wouter Bijnsdorp <wouterb98@gmail.com>
2021-02-09 14:52:58 +0100

    Removed unneeded comments

--- index.js
+++ index.js
@@ -22,21 +22,3 @@ app.use(function (req, res, next){
     res.status(404).send("Error 404 - Sorry, can't find this page for you!")
 });
 
-
-
-/*
-  //Respond to a Post request to the /user route:
-app.post('/', function (req, res) {
-    res.send('Got a POST request')
-  })
-  //Respond to a PUT request to the /user route:
-  
-  app.put('/user', function (req, res) {
-    res.send('Got a PUT request at /user')
-  })
-  //Respond to a DELETE request to the /user route:
-  
-  app.delete('/user', function (req, res) {
-    res.send('Got a DELETE request at /user')
-  })
-  */
