e1dd19e3efa926fa9de1264c6ceecca1392819b7
Wouter Bijnsdorp <wouterb98@gmail.com>
2021-02-09 13:16:06 +0100

    Added 404 basic

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/',(req,res) =>{
+app.get('/',(req, res) =>{
     res.send("Hello World")
 });
 
 app.listen(port, ()=>{
     console.log('Example app listening on port ${port}!')
+});
+
+app.arguments(function (req, res, next){
+    res.status(404).send("Sorrt, can't find it!")
 });
