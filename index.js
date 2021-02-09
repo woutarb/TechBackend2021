cf15ce078ebc24f813281ef3050b89441d7712e9
Wouter Bijnsdorp <wouterb98@gmail.com>
2021-02-09 15:35:29 +0100

    Added page, edited main page

--- index.js
+++ index.js
@@ -3,11 +3,15 @@ const app = express()
 const port = 3000
 
 app.get('/',(req, res) =>{
-    res.send("Hello World")
+    res.send("Hello World - Welcome to the main page")
 });
 
 app.get('/profile',(req, res) =>{
     res.send("Your profile")
+});
+
+app.get('/user',(req, res) =>{
+    res.send("A different profile")
 });
 
 app.listen(port, ()=>{
