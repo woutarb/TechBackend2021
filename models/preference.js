b69e8114cc7919061d39f5a6b4373535087f893a
Wouter Bijnsdorp <wouterb98@gmail.com>
2021-03-10 21:28:44 +0100

    Downgrade, natural language, min-max
    Downgrade mongoose to fix things, fix pug template to be more readable, include new age system

--- models/preference.js
+++ models/preference.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const schemeName = 'Preference';
 const preferenceSchema = new mongoose.Schema({
     genderPref: { type: String, required: true },
-    agePref: { type: Number, required: true },
+    minAgePreference: { type: Number, required: true },
+    maxAgePreference: { type: Number, required: true },
     percentOverlap: { type: Number, required: true },
 });
 module.exports.Preference = mongoose.model(schemeName,preferenceSchema);
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const pug = require('pug');
 const slug = require('slug');
 require('dotenv').config()
-const bodyParser = require('body-parser');
+//const bodyParser = require('body-parser');
 const path = require('path');
 const multer = require('multer');
 const MongoClient = require('mongodb').MongoClient
@@ -17,11 +17,10 @@ let userId;
 app.set('views', __dirname + '/views');
 app.set('view engine', 'pug')
 app.use(express.static(path.join(__dirname, 'Public')));
-app.use(bodyParser.urlencoded({ extended: true }))
+//app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: false }));
 
 app.use(express.json());
-
-app.use(express.urlencoded({ extended: false }));
 
 const connectionString = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.o0u7k.mongodb.net/Cluster0?retryWrites=true&w=majority`
 
@@ -37,17 +36,22 @@ app.get('/',(req, res) =>{
 
 app.get('/home',(req, res) =>{
     // userId I can reach
+  
     if(typeof userId === "string"){
         let currentPrefs= prefModels.Preference.findById(userId,(err, preferenceData)=>{
-            userModels.User.find()
+            userModels.User.find({})
             .where('gender').equals(preferenceData.genderPref)
-            .where('age').equals(preferenceData.agePref)
+            .where('age').gte(preferenceData.minAgePref)
+            .where('age').lte(preferenceData.maxAgePref)
             .where('beers').gt(preferenceData.percentOverlap)
             .exec().then((Users,err)=>{
                 res.render('index',{userList: Users})
             console.log('users: ' + Users);
             console.log('err: ' + err);
             console.log('genderpref: ' + preferenceData.genderPref);
+            console.log('age: ' + preferenceData.agePref);
+            console.log('tasteoverlapandshizzle: ' + preferenceData.percentOverlap);
+
             });
         });
        
@@ -67,7 +71,8 @@ app.get('/preferences',(req, res) =>{
 app.post('/preferences', (req, res) => {
     let userPref={
         genderPref: req.body.genderOther,
-        agePref: req.body.agePreference,
+        maxAgePref: req.body.minAgePreference,
+        minAgePref: req.body.maxAgePreference,
         percentOverlap: req.body.percent
     }
     const model = new prefModels.Preference(userPref);
